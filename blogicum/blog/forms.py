db4bebac1f1264e0708a7cc668408fa084008301
unknown <you@example.com>
2026-03-30 23:23:08 +0300

    osjcojv

---
+++ blogicum/blog/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+from .models import Comment, Post
+
+User = get_user_model()
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = (
+            "title",
+            "text",
+            "pub_date",
+            "location",
+            "category",
+            "image",
+        )
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ("text",)
+
+
+class RegistrationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ("username", "first_name", "last_name", "email")
+
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name", "username", "email")
